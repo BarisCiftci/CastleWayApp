cb0db242fd4c89579e7b8c5499e981d0412f2d34
BarisCiftci <barishciftci@gmail.com>
2023-07-27 22:44:45 +0200

    Initial Commit

---
+++ CastleWayApp/CastleWayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastleWayAppApp.swift
+//  CastleWayApp
+//
+//  Created by Baris Ciftci on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CastleWayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
